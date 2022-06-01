0d1eaef8b98d4e14f4c6c8217367afe0fe75bd29
Ilnur786 <Ilnur-qwerty@mail.ru>
2022-06-02 00:58:11 +0300

    Change logic: now this module has: create_token, get_token and decode_jwt functions. If get_token func can't find jwt_token field, create_token func create token and write it into config.ini

--- all_neccessary_files/jwt_token.py
+++ all_neccessary_files/jwt_token.py
@@ -3,22 +3,64 @@ import configparser
 import time
 
 
-def encode_jwt(user_id, text, timestamp=None):
+# def encode_jwt(user_id, text, timestamp=None):
+# 	config = configparser.ConfigParser()
+# 	config.read("config.ini")
+# 	key = config['JWT']['key']
+# 	algorithm = config['JWT']['algorithm']
+# 	if timestamp is None:
+# 		timestamp = str(time.time())
+# 	payload = {"user_id": user_id, "text": text, "timestamp": timestamp}
+# 	encoded = jwt.encode(payload, key, algorithm)
+# 	return encoded
+#
+#
+# def decode_jwt(encoded):
+# 	config = configparser.ConfigParser()
+# 	config.read("config.ini")
+# 	key = config['JWT']['key']
+# 	algorithm = config['JWT']['algorithm']
+# 	return jwt.decode(encoded, key, algorithm)
+
+### А ВООБЩЕ ПРОСТО НАДО СГЕНЕРИРОВАТЬ ТОКЕН И СДЕЛАТЬ ЕГО ПЕРЕМЕННОЙ СРЕДЫ И ВСЕ! ###
+def create_token(config_path='config.ini'):
+	config = configparser.ConfigParser()
+	config.read(config_path)
+	key = config['JWT']['key']
+	algorithm = config['JWT']['algorithm']
+	jwt_token = jwt.encode({"post_message_confirm": True}, key, algorithm)
+	with open(config_path, 'a') as f:
+		f.write(f'\njwt_token = {jwt_token}')
+	return jwt_token
+
+
+def get_token(config_path='config.ini'):
+	config = configparser.ConfigParser()
+	config.read(config_path)
+	jwt_token = config['JWT'].get('jwt_token')
+	if jwt_token is None:
+		jwt_token = create_token(config_path)
+	return jwt_token
+
+
+def decode_jwt(encoded, config_path='config.ini'):
 	config = configparser.ConfigParser()
 	config.read("config.ini")
 	key = config['JWT']['key']
 	algorithm = config['JWT']['algorithm']
-	if timestamp is None:
-		timestamp = str(time.time())
-	payload = {"user_id": user_id, "text": text, "timestamp": timestamp}
-	encoded = jwt.encode(payload, key, algorithm)
-	return encoded
+	try:
+		jwt_token = jwt.decode(encoded, key, algorithm)["post_message_confirm"]
+	except jwt.exceptions.InvalidSignatureError:
+		jwt_token = False
+	except jwt.exceptions.InvalidAlgorithmError:
+		jwt_token = False
+	return jwt_token
 
 
-def decode_jwt(encoded):
-	config = configparser.ConfigParser()
-	config.read("config.ini")
-	key = config['JWT']['key']
-	algorithm = config['JWT']['algorithm']
-	return jwt.decode(encoded, key, algorithm)
-
+if __name__ == '__main__':
+	user = 121247482
+	text = 'sdhnfjkewfewf'
+	with open('test.txt', 'a+', ) as f:
+		f.seek(0)
+		print(f.read())
+		f.write('\ntoken')
